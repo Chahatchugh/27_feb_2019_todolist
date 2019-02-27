94672502ffbf0c9b16325a739a68b3c1ab3347d3
Chahatchugh <32201339+Chahatchugh@users.noreply.github.com>
2019-02-27 20:55:04 +0530

    Update 27_feb_2019_ToDoList.js

--- 27_feb_2019_ToDoList.js
+++ 27_feb_2019_ToDoList.js
@@ -59,7 +59,7 @@ function createTable () {
 document.body.appendChild(table)
 table.setAttribute( "border", "2" )
 //From stack overflow 
-const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+/*const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
 const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
     v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
     )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
@@ -68,5 +68,5 @@ const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
     Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
         .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
         .forEach(tr => table.appendChild(tr) );
-})));
+})));*/
 }
